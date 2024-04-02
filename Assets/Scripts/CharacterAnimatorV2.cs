3187e5b2a80197f5f2bd18fd7e4e22b13a1d7387
SergeyVolik <volik.sergey112@gmail.com>
2024-04-02 14:23:31 +0300

    :sparkles: 4 direction movement

--- Assets/Scripts/CharacterAnimatorV2.cs
+++ Assets/Scripts/CharacterAnimatorV2.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class CharacterAnimatorV2 : MonoBehaviour
 {
     private Animator m_Animator;
-    private ICharacterInput m_CharacterInput;
+    private CustomCharacterController m_CharacterInput;
+    private Transform m_ControllerTrans;
 
     private void Awake()
     {
        m_Animator = GetComponent<Animator>();
-        m_CharacterInput = GetComponentInParent<ICharacterInput>();
+        m_CharacterInput = GetComponentInParent<CustomCharacterController>();
+        m_ControllerTrans = m_CharacterInput.transform;
     }
     public void Move(Vector2 vector)
     {
@@ -17,6 +19,21 @@ public class CharacterAnimatorV2 : MonoBehaviour
 
     private void Update()
     {
+        var delta = Time.deltaTime;
+
         Move(m_CharacterInput.MoveInput);
+
+        if (m_CharacterInput.canAim)
+        {
+            m_Animator.SetBool("IsAim", m_CharacterInput.IsAiming);
+
+            var vector = m_ControllerTrans.InverseTransformDirection(new Vector3(m_CharacterInput.MoveInput.x, 0, m_CharacterInput.MoveInput.y)).normalized;
+
+            var forwardAmount = vector.z;
+            var turnAmount = vector.x;
+
+            m_Animator.SetFloat("Turn", turnAmount, 0.1f, delta);
+            m_Animator.SetFloat("Forward", forwardAmount, 0.1f, delta);
+        }
     }
 }
--- Assets/Scripts/CustomCharacterController.cs
+++ Assets/Scripts/CustomCharacterController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,21 +14,52 @@ public class CustomCharacterController : MonoBehaviour, ICharacterInput
     private Rigidbody m_Rb;
     private Transform m_Transform;
     public float speed;
+
+    public bool canAim;
+    public bool IsAiming;
+
+    public LayerMask CastMask;
+    public Vector2 AimVector;
+    public float castRadius;
     public Vector2 MoveInput { get => m_MoveInput; set => m_MoveInput = value; }
+    RaycastHit[] results;
 
     private void Awake()
     {
         m_Rb = GetComponent<Rigidbody>();
         m_Transform = transform;
+        results = new RaycastHit[10];
     }
     private void Update()
     {
         var normalized = MoveInput.normalized;
         var moveVec = normalized * speed;
         m_Rb.velocity = new Vector3(moveVec.x, 0, moveVec.y);
+
+        var currentPos = m_Transform.position;
+
         if (MoveInput != Vector2.zero)
         {
             m_Transform.forward = new Vector3(normalized.x, 0, normalized.y);
+        }
+
+        if (canAim)
+        {
+            int count = Physics.SphereCastNonAlloc(currentPos, castRadius, Vector3.up, results, 10, CastMask);
+            IsAiming = count != 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                var resourt = results[i];
+                var point = resourt.transform.position;
+                point.y = currentPos.y;
+                var vector = point - currentPos;
+
+                AimVector = new Vector2(vector.x, vector.z);
+                m_Transform.forward = vector.normalized;
+
+                break;
+            }
         }
     }
 }
